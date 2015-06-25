a2d3a6c7445c46dec8c275b66e432eb369ce51e1
cheng cui <cheng.cui@avepoint.com>
2015-06-25 14:58:20 +0800

    Add new dll

--- CSOM File Add Test/Program.cs
+++ CSOM File Add Test/Program.cs
@@ -17,14 +17,16 @@ namespace CSOM_File_Add_Test
         {
             ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, errors) => true;
 
-            Write(AddFileWithBytes);
-            Write(AddFileWithStream);
-            Write(AddFileWithSaveBinaryDirect);
-            Write(AddFileWithSaveBytes);
-            Write(AddFileWithSaveStream);
+            AddFileWithContinueUpload();
 
-            Write(AddLargeFileWithStream);
-            Write(AddLargeFileWithSaveBinaryDirect);
+            //Write(AddFileWithBytes);
+            //Write(AddFileWithStream);
+            //Write(AddFileWithSaveBinaryDirect);
+            //Write(AddFileWithSaveBytes);
+            //Write(AddFileWithSaveStream);
+
+            //Write(AddLargeFileWithStream);
+            //Write(AddLargeFileWithSaveBinaryDirect);
         }
 
 
@@ -193,5 +195,142 @@ namespace CSOM_File_Add_Test
 
             context.ExecuteQuery();
         }
+
+        private static void AddFileWithContinueUpload()
+        {
+            string fileName = "D:\\Reflector_8.3.0.93_XiaZaiBa.zip";
+
+
+
+            ClientContext context = new ClientContext("https://simmon6.sharepoint.com/sites/Test1");
+
+            SecureString passworSecureString = new SecureString();
+
+            "1qaz2wsxE".ToCharArray().ToList().ForEach(passworSecureString.AppendChar);
+
+            context.Credentials = new SharePointOnlineCredentials("cheng.cui@simmon6.onmicrosoft.com", passworSecureString);
+
+            ClientResult<long> bytesUploaded = null;
+
+            FileStream fs = null;
+            File uploadFile = null;
+            Guid uploadId = Guid.NewGuid();
+            try
+            {
+                fs = System.IO.File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    byte[] buffer = new byte[1024 * 1024];
+                    Byte[] lastBuffer = null;
+                    long fileoffset = 0;
+                    long totalBytesRead = 0;
+                    int bytesRead;
+                    bool first = true;
+                    bool last = false;
+
+                    // Read data from filesystem in blocks 
+                    while ((bytesRead = br.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        totalBytesRead = totalBytesRead + bytesRead;
+
+                        // We've reached the end of the file
+                        if (totalBytesRead == fs.Length)
+                        {
+                            last = true;
+                            // Copy to a new buffer that has the correct size
+                            lastBuffer = new byte[bytesRead];
+                            Array.Copy(buffer, 0, lastBuffer, 0, bytesRead);
+                        }
+
+                        if (first)
+                        {
+                            using (MemoryStream contentStream = new MemoryStream())
+                            {
+                                // Add an empty file.
+                                FileCreationInformation fileInfo = new FileCreationInformation();
+                                fileInfo.ContentStream = contentStream;
+                                fileInfo.Url = Path.GetFileName(fileName);
+                                fileInfo.Overwrite = true;
+
+                                Folder folder = context.Web.GetFolderByServerRelativeUrl("/sites/Test1/Test Library");
+
+
+                                uploadFile = folder.Files.Add(fileInfo);
+
+                                // Start upload by uploading the first slice. 
+                                using (MemoryStream s = new MemoryStream(buffer))
+                                {
+                                    // Call the start upload method on the first slice
+                                    bytesUploaded = uploadFile.StartUpload(uploadId, s);
+                                    context.ExecuteQuery();
+                                    // fileoffset is the pointer where the next slice will be added
+                                    fileoffset = bytesUploaded.Value;
+                                    Console.WriteLine("fileoffset:" + fileoffset);
+                                }
+
+                                // we can only start the upload once
+                                first = false;
+                            }
+                        }
+                        else
+                        {
+                            // Get a reference to our file
+                            uploadFile = context.Web.GetFileByServerRelativeUrl("/sites/Test1/Test Library" + System.IO.Path.AltDirectorySeparatorChar + Path.GetFileName(fileName));
+
+                            if (last)
+                            {
+                                // Is this the last slice of data?
+                                using (MemoryStream s = new MemoryStream(lastBuffer))
+                                {
+                                    // End sliced upload by calling FinishUpload
+                                    uploadFile = uploadFile.FinishUpload(uploadId, fileoffset, s);
+                                    context.ExecuteQuery();
+                                    Console.WriteLine("fileoffset:" + fileoffset);
+                                    // return the file object for the uploaded file
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                using (MemoryStream s = new MemoryStream(buffer))
+                                {
+                                    // Continue sliced upload
+                                    bytesUploaded = uploadFile.ContinueUpload(uploadId, fileoffset, s);
+                                    context.ExecuteQuery();
+                                    // update fileoffset for the next slice
+                                    fileoffset = bytesUploaded.Value;
+                                    Console.WriteLine("fileoffset:" + fileoffset);
+                                }
+                            }
+                        }
+
+                    } // while ((bytesRead = br.Read(buffer, 0, buffer.Length)) > 0)
+                }
+            }
+            finally
+            {
+                //if (fs != null)
+                //{
+                //    fs.Dispose();
+                //}
+            }
+
+            uploadFile = context.Web.GetFileByServerRelativeUrl("/sites/Test1/Test Library" + System.IO.Path.AltDirectorySeparatorChar + Path.GetFileName(fileName));
+
+            ConditionalScope conditionScope = new ConditionalScope(context, () => uploadFile.Exists, true);
+            using (conditionScope.StartScope())
+            {
+                using (conditionScope.StartIfTrue())
+                {
+                    context.Load(uploadFile);
+
+                    context.Load(uploadFile.ListItemAllFields);
+                }
+            }
+            context.ExecuteQuery();
+
+
+        }
+
     }
 }
