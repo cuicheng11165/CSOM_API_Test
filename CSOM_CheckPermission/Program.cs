f175ca51af7bd5153726c0b836b136ef471c3fd4
Simmon Cui <chengcui@avepoint.com>
2018-09-18 15:23:08 +0800

    add permission check

---
+++ CSOM_CheckPermission/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSOM_CheckPermission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClientContext context = new ClientContext("https://bigapp.sharepoint.com/sites/simmon1456");
+
+            SecureString passworSecureString = new SecureString();
+
+            System.IO.File.ReadAllText("password.txt").ToCharArray().ToList().ForEach(passworSecureString.AppendChar);
+
+            context.Credentials = new SharePointOnlineCredentials("simmon@baron.space", passworSecureString);
+
+            var basePermission = context.Web.GetUserEffectivePermissions("i:0#.f|membership|simmon@baron.space");
+            context.ExecuteQuery();
+
+            var contributorType = context.Web.RoleDefinitions.GetByType(RoleType.Contributor);
+
+            context.Load(contributorType);
+            context.ExecuteQuery();
+
+            var checkHigh = basePermission.Value.HasPermissions(48, 134287360);
+
+
+        }
+    }
+}
