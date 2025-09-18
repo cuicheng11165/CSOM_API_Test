d1cac1fa6ed08f8d7e0418b342b6c0e7fba62e4b
chengcui <cheng.cui@avepoint.com>
2025-09-18 15:05:21 +0800

    Remove files

--- Test1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Microsoft.Azure.Services.AppAuthentication;
-
-var ss =new AzureServiceTokenProvider();
-var sss=ss.GetAccessTokenAsync("https://management.azure.com/").GetAwaiter().GetResult();
-
-
-Console.ReadLine();
